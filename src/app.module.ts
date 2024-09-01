113809b2201d81ce4e2a158b89557d55f06ef0c9
Sebastian <sebasrojasm1@gmail.com>
2024-08-31 23:27:20 -0500

    The Entty  was created with the title, body, time to creation properties, documentation and validations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-
 import { NotesModule } from './notes/notes.module';
+import dbConfig from 'config/persistence/db-config';
+import { ConfigModule } from '@nestjs/config';
+import { NotesController } from './notes/controllers/notes.controller';
+import { NotesService } from './notes/services/notes.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Notes, noteSchema } from './notes/entities/note.entity';
 
 @Module({
-  imports: [NotesModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [dbConfig],
+      envFilePath: '.env',
+    }),
+    MongooseModule.forFeature([{ name: Notes.name, schema: noteSchema }]),
+    NotesModule
+  ],
+  controllers: [NotesController],
+  providers: [NotesService],
 })
 export class AppModule {}
